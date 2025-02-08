19abd6172d1b50fbbe9e4bb388b529701efd74ab
FraserDavies <fd2010@hw.ac.uk>
2025-02-08 21:38:18 +0000

    added new devices to site

--- javascript/device-type.js
+++ javascript/device-type.js
@@ -1,9 +1,21 @@
+// All types of devices in system
+// Lights
+// A/C
+// Speaker
+// Projector
+// Laptop
+// Printer
+// Coffee Machine
+// Monitor
+// Server
+
 // Device types with inverted icons (for active/selected state)
 export const deviceTypesNotInverted = {
     'Lights': './images/icons/Lights-inverted.png',
     'A/C': './images/icons/AC(MIT)-inverted.png',
     'Speaker': './images/icons/Speaker(MIT)-inverted.png',
-    'Projector': './images/icons/Projector-inverted.png'
+    'Projector': './images/icons/Projector-inverted.png',
+    'Laptop': './images/icons/laptop-inverted.png'
 };
 
 // Device types with regular icons (for inactive/unselected state)
@@ -11,5 +23,6 @@ export const deviceTypes = {
     'Lights': './images/icons/Lights.png',
     'A/C': './images/icons/AC(MIT).png',
     'Speaker': './images/icons/Speaker(MIT).png',
-    'Projector': './images/icons/Projector.png'
+    'Projector': './images/icons/Projector.png',
+    'Laptop': './images/icons/laptop icon(MIT).png'
 };
