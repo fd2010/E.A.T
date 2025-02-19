a1b775a0092631b916585e5a0e2a9bd7f734fe84
muskaangarg280 <muskaangarg280@gmail.com>
2025-02-19 03:21:05 +0000

    js

--- javascript/energyData.js
+++ javascript/energyData.js
@@ -5,15 +5,49 @@ export const timeLabels = {
 };
 
 export const energyData = {
-    daily: [2, 3, 5, 4, 5],
-    weekly: [30, 40, 35, 50, 45, 38, 42],
-    monthly: [200, 220, 210, 230]
+    "Meeting Room": {
+        daily: [5, 8, 10, 12, 6],
+        weekly: [40, 50, 45, 60, 55, 48, 52],
+        monthly: [150, 170, 160, 180]
+    },
+    "Common Areas": {
+        daily: [3, 5, 7, 6, 4],
+        weekly: [30, 35, 40, 38, 36, 33, 31],
+        monthly: [100, 120, 130, 110]
+    },
+    "Workstations": {
+        daily: [6, 9, 11, 14, 7],
+        weekly: [45, 55, 50, 65, 60, 52, 57],
+        monthly: [180, 200, 190, 210]
+    },
+    "Special": {
+        daily: [7, 10, 12, 15, 8],
+        weekly: [50, 60, 55, 70, 65, 58, 62],
+        monthly: [200, 220, 210, 230]
+    }
 };
 
 export const costData = {
-    daily: [10, 15, 20, 18, 22],
-    weekly: [100, 120, 110, 140, 130, 125, 135],
-    monthly: [400, 450, 420, 460]
+    "Meeting Room": {
+        daily: [10, 16, 20, 24, 12],
+        weekly: [80, 100, 90, 120, 110, 96, 104],
+        monthly: [300, 340, 320, 360]
+    },
+    "Common Areas": {
+        daily: [6, 10, 14, 12, 8],
+        weekly: [60, 70, 80, 76, 72, 66, 62],
+        monthly: [200, 240, 260, 220]
+    },
+    "Workstations": {
+        daily: [12, 18, 22, 28, 14],
+        weekly: [90, 110, 100, 130, 120, 104, 114],
+        monthly: [360, 400, 380, 420]
+    },
+    "Special": {
+        daily: [14, 20, 24, 30, 16],
+        weekly: [100, 120, 110, 140, 130, 116, 124],
+        monthly: [400, 440, 420, 460]
+    }
 };
 
 export const areaData = {
