b6389e86a712e978a53eaea5c566fbc3161c8866
FraserDavies <fd2010@hw.ac.uk>
2025-02-20 23:07:23 +0000

    Update firebase-config.js

--- database/firebase-config.js
+++ database/firebase-config.js
@@ -4,16 +4,28 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAOw5Zc-E3Gq74lGYAVHcpkgNnNrumrdu0",
+//     authDomain: "energy-analysis-tool.firebaseapp.com",
+//     databaseURL: "https://energy-analysis-tool-default-rtdb.europe-west1.firebasedatabase.app",
+//     projectId: "energy-analysis-tool",
+//     storageBucket: "energy-analysis-tool.appspot.com",
+//     messagingSenderId: "333108098273",
+//     appId: "1:333108098273:web:ddabe077f6be647ad1b3a0",
+//     measurementId: "G-CP2WDCMRM9"
+// };
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAOw5Zc-E3Gq74lGYAVHcpkgNnNrumrdu0",
     authDomain: "energy-analysis-tool.firebaseapp.com",
     databaseURL: "https://energy-analysis-tool-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "energy-analysis-tool",
-    storageBucket: "energy-analysis-tool.appspot.com",
+    storageBucket: "energy-analysis-tool.firebasestorage.app",
     messagingSenderId: "333108098273",
     appId: "1:333108098273:web:ddabe077f6be647ad1b3a0",
     measurementId: "G-CP2WDCMRM9"
-};
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
